f1799ca9b1df2c0100ce945ff44f7415621973a0
HungryAmoeba <70213522+HungryAmoeba@users.noreply.github.com>
2021-08-18 15:22:36 -0400

    Add files via upload

---
+++ pytracking/evaluation/local.py
@@ -0,0 +1,28 @@
+from pytracking.evaluation.environment import EnvSettings
+
+def local_env_settings():
+    settings = EnvSettings()
+
+    # Set your local paths here.
+
+    settings.davis_dir = ''
+    settings.got10k_path = ''
+    settings.got_packed_results_path = ''
+    settings.got_reports_path = ''
+    settings.lasot_path = '/home/warp/Documents/LaSOTTesting'
+    settings.network_path = '/home/warp/Documents/Charles/pytracking/pytracking/networks/'    # Where tracking networks are stored.
+    settings.nfs_path = ''
+    settings.otb_path = ''
+    settings.result_plot_path = '/home/warp/Documents/Charles/pytracking/pytracking/result_plots/'
+    settings.results_path = '/home/warp/Documents/Charles/pytracking/pytracking/tracking_results/'    # Where to store tracking results
+    settings.segmentation_path = '/home/warp/Documents/Charles/pytracking/pytracking/segmentation_results/'
+    settings.tn_packed_results_path = ''
+    settings.tpl_path = ''
+    settings.trackingnet_path = ''
+    settings.uav_path = ''
+    settings.vot_path = '/home/warp/Documents/Charles/vot-workspace/sequences'
+    settings.youtubevos_dir = ''
+    settings.fish_path = '/home/warp/Documents/Charles/MADv0_ST'
+    settings.bagging_dir = '/home/warp/Documents/Charles/pytracking/pytracking/bagged_summary_images'
+
+    return settings
