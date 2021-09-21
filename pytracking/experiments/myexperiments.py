d02268edd268c7970b58171b7eeef2526faf4ec2
HungryAmoeba <70213522+HungryAmoeba@users.noreply.github.com>
2021-09-21 19:14:00 -0400

    Update myexperiments.py

--- pytracking/experiments/myexperiments.py
+++ pytracking/experiments/myexperiments.py
@@ -104,3 +104,98 @@ def fish_interactive():
     dataset = get_dataset('fish')
 
     return trackers, dataset
+
+def vot_summary_update():
+    trackers = trackerlist('dimp_summary', 'super_dimp_15_summary_update', range(1))
+
+    dataset = get_dataset('vot')
+
+    return trackers, dataset
+
+def vot_summary_update_no_20():
+    trackers = trackerlist('dimp_summary', 'super_dimp_15_summary_update_no_20', range(1)) + \
+               trackerlist('dimp_summary', 'super_dimp_15_summary_update_no_20_1step', range(1)) + \
+               trackerlist('dimp_summary', 'super_dimp_15_summary_update', range(1))
+
+    dataset = get_dataset('vot')
+
+    return trackers, dataset
+
+def vot_summary_update_with_live():
+    trackers = trackerlist('dimp_summary', 'super_dimp_15_summary_update', range(1))
+
+    dataset = get_dataset('vot')
+
+    return trackers, dataset
+
+def comprehensive_xdimp_test():
+    trackers = trackerlist('dimp_summary', 'super_dimp_15_summary_update_no_20', range(1))
+
+    dataset = get_dataset('fish', 'vot', 'otb', 'lasot')
+
+    return trackers, dataset
+
+def baseline_dimp_otb():
+    trackers = trackerlist('dimp_original', 'super_dimp', range(1))
+
+    dataset = get_dataset('otb')
+
+    return trackers, dataset
+
+def baseline_dimp_lasot():
+    trackers = trackerlist('dimp_original', 'super_dimp', range(1))
+
+    dataset = get_dataset('lasot')
+
+    return trackers, dataset
+
+def otb_rerun():
+    trackers = trackerlist('dimp_original', 'super_dimp', range(1)) + \
+               trackerlist('dimp_summary', 'super_dimp_15_summary_update_no_20', range(1))
+
+    dataset = get_dataset('otb')
+
+    return trackers, dataset
+
+def old_update_xdimp():
+    trackers = trackerlist('dimp_summary', 'super_dimp_15', range(1))
+
+    dataset = get_dataset('fish', 'vot', 'otb', 'lasot')
+
+    return trackers, dataset
+
+def RLT_dimp_comprehensive_test():
+    trackers = trackerlist('RLT_XDiMP', 'new', range(1)) + \
+               trackerlist('RLT_dimp', 'new', range(1))
+
+    dataset = get_dataset('fish', 'vot', 'otb', 'lasot')
+
+    return trackers, dataset
+
+def vot2020_ST_test():
+    trackers = trackerlist('dimp_original', 'super_dimp', range(1)) + \
+               trackerlist('dimp_summary', 'super_dimp_15', range(1))
+
+    dataset = get_dataset('vot_2020')
+
+    return trackers, dataset
+
+def UAV123_test():
+    trackers = trackerlist('dimp_original', 'super_dimp', range(1)) + \
+               trackerlist('dimp_summary', 'super_dimp_15', range(1))
+
+    dataset = get_dataset('uav')
+
+    return trackers, dataset
+
+def vot_summary_size_test():
+    trackers = trackerlist('dimp_summary', 'super_dimp_5', range(1)) + \
+               trackerlist('dimp_summary', 'super_dimp_10', range(1)) + \
+               trackerlist('dimp_summary', 'super_dimp_15', range(1)) + \
+               trackerlist('dimp_summary', 'super_dimp_20', range(1)) + \
+               trackerlist('dimp_summary', 'super_dimp_25', range(1)) + \
+               trackerlist('dimp_summary', 'super_dimp_30', range(1))
+
+    dataset = get_datset('vot_2020', 'vot')
+
+    return trackers, dataset 
