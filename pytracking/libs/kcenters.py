8f7cc7447c3e7594afcab7759eb98a3dcfed151b
Levi Cai <cail@mit.edu>
2022-03-20 17:43:35 -0400

    Adding kcenters updates

--- pytracking/libs/kcenters.py
+++ pytracking/libs/kcenters.py
@@ -12,13 +12,22 @@ class IndexedTensor:
     def get_index(self):
         return self.original_index
 
+def l2_dist(A, B):
+    return torch.norm(A-B)
+
+def l2_normalised_dist(A, B):
+    return torch.norm(A/torch.norm(A) - B/torch.norm(B))
+
+def cosine_dist(A, B):
+    return 1-torch.dot(A.flatten(), B.flatten())/(torch.norm(A) * torch.norm(B))
+
 def Tensor_to_IndexedTensor(input_tensor):
   IndexedTensorList = list()
   for index, individual_tensor in enumerate(input_tensor):
     IndexedTensorList.append(IndexedTensor(individual_tensor, index))
   return IndexedTensorList
 
-def distance_cost(summary_set, observation, distance_function = torch.norm):
+def distance_cost(summary_set, observation, distance_function = l2_dist):
   '''This function takes as input a summary set and a new observation and returns the distance and
   index of the item in the summary set that is the closet item to the new observation'''
 
@@ -33,12 +42,12 @@ def distance_cost(summary_set, observation, distance_function = torch.norm):
 
    return curr_min_distance, best_index '''
 
-  distances = torch.tensor([distance_function(observation-x) for x in summary_set])
+  distances = torch.tensor([distance_function(observation,x) for x in summary_set])
   return torch.min(distances), torch.argmin(distances)
 
 
 
-def return_furthest_point(summary_set, observation_set, distance_function = torch.norm):
+def return_furthest_point(summary_set, observation_set, distance_function = l2_dist):
   '''This function works in more or less an offline fashion. It takes as input an entire summary
   set and the entire set of observations and returns the index of the observation set that is
   farthest away from samples in the current summary '''
@@ -52,7 +61,7 @@ def return_furthest_point(summary_set, observation_set, distance_function = torc
 
   return furthest_min_distance, best_index
 
-def score_observation(summary_set, observation, distance_function = torch.norm):
+def score_observation(summary_set, observation, distance_function = l2_dist):
   '''Takes the summary set as input along with an observation and returns the
   minimum distance to any point in the summary set '''
   '''
@@ -64,15 +73,15 @@ def score_observation(summary_set, observation, distance_function = torch.norm):
 
   return min_distance
   '''
-  return torch.min(torch.tensor([distance_function(x - observation) for x in summary_set]))
+  return torch.min(torch.tensor([distance_function(x, observation) for x in summary_set]))
 
-def threshold_cost(summary_set, distance_function = torch.norm):
+def threshold_cost(summary_set, distance_function = l2_dist):
   '''Calculates the threshold as the mean score of each of the samples currently in the summary '''
   length = summary_set.size()[0]
   total_score = 0
   for index_, summary_instance in enumerate(summary_set):
     #set_without_summary_instance = summary_set[np.r_[:index_, (index_ + 1):length], :]
-    best_distance = score_observation(summary_set[np.r_[:index_, (index_ + 1):length], :], summary_set[index_])
+    best_distance = score_observation(summary_set[np.r_[:index_, (index_ + 1):length], :], summary_set[index_], distance_function=distance_function)
     total_score = total_score + best_distance
 
   if total_score == 0 or length == 0:
@@ -85,7 +94,7 @@ def threshold_cost(summary_set, distance_function = torch.norm):
     final_threshold = total_score/length
   return final_threshold
 
-def extremumsummary(observation_set, num_observations):
+def extremumsummary(observation_set, num_observations, distance_function=l2_dist):
   '''Computes a summary as a subset of input samples (observationconvert tensor to tuple_set) by greedily picking the samples
   farthest away from sapmles in the current summary. k is the desired number of samples in the summary'''
   # initialize the summary set with a random index:
@@ -99,7 +108,7 @@ def extremumsummary(observation_set, num_observations):
   while summary.size()[0] < num_observations:
     #index = argmax_i min_j d(Zi, Sj)
     #choose samples to add to summary by finding samples farthest away from samples in current summary
-    _, best_index = return_furthest_point(summary, observation_set)
+    _, best_index = return_furthest_point(summary, observation_set, distance_function=distance_function)
     summary = torch.cat((summary, torch.reshape(observation_set[best_index], (1,) + tuple(torch.tensor(observation_set.size()[1:]).numpy()))), dim = 0)
     observation_set = observation_set[np.r_[:best_index, (best_index + 1):observation_set.size()[0]], ]
 
@@ -118,9 +127,12 @@ def online_summary_update(summary_set, observation, k):
 
 def k_online_summary_update_index(summary_set, observation, k, threshold = None, indices_to_keep = None):
     """
+    NOT DONE
     summary_set torch tensor, first dim is number of elements
     k is the maximum summary size
     """
+    raise NotImplementedError
+
     summary_size = summary_set.size()[0]
 
     # Initialize a threshold cost if none-existent
@@ -137,7 +149,7 @@ def k_online_summary_update_index(summary_set, observation, k, threshold = None,
     # Not added
     return -1, threshold
 
-def online_summary_update_index_extremum(summary_set, observation, k, threshold = None):
+def online_summary_update_index_extremum(summary_set, observation, k, threshold = None, distance_function=l2_dist):
     '''Returns the index of the entry in the original summary set to be replaced when
     the summary size == k upon addition of the new observation '''
     length = summary_set.size()[0]
@@ -151,35 +163,32 @@ def online_summary_update_index_extremum(summary_set, observation, k, threshold 
         #print(f"threshold is {threshold}")
     #threshold = threshold_cost(summary_set)
 
-    score = score_observation(summary_set, observation)
-    print("score: ", score)
-    print("threshold: ", threshold)
+    score = score_observation(summary_set, observation, distance_function=distance_function)
+
+    #print(f"Score, Threshold: {score} | {threshold}")
+
+    # New observation does not meet threshold, do not add
+    if score <= threshold:
+        return -1, threshold
+
+    # New observation meets threshold, set is not max size, simply add it
     if score > threshold and length < k:
         return length, threshold
 
-    if score > threshold:
-      #add to summary set
-      #doing the list comprehension here might be superfluous
-      dummy_summary_set = [x for x in torch.cat((summary_set, observation), dim = 0)]
-    else:
-        return -1, threshold
-        #find something to return that won't break things.
-
+    # New observation meets threshold, but set is full, need to prune and run extremum summary
+    dummy_summary_set = [x for x in torch.cat((summary_set, observation), dim = 0)]
     dummy_summary_set = Tensor_to_IndexedTensor(dummy_summary_set)
     if len(dummy_summary_set) > k:
       #we are getting problems because of something in extremum_summary
-      indices_in_new_summary = extremumsummary_indexes(dummy_summary_set, k)
+      indices_in_new_summary = extremumsummary_indexes(dummy_summary_set, k, distance_function=distance_function)
       indices_before = set(range(k))
       index_to_remove = indices_before - indices_in_new_summary
-      '''print(f"indices before is {indices_before} \n ________________")
-      print(f"indices in new summary is {indices_in_new_summary}")
-      print(f"the index_to_remove is {index_to_remove} \n ________________")'''
 
       return index_to_remove.pop(), threshold #, dummy_summary_set
 
     return -1, threshold
 
-def extremumsummary_indexes(observation_set, num_observations):
+def extremumsummary_indexes(observation_set, num_observations, distance_function=l2_dist):
   '''Computes a summary as a subset of input samples by greedily picking the samples
   farthest away from sapmles in the current summary. k is the desired number of samples in the summary. observation_set is assumed to be
   a list of Indexed Tensors'''
@@ -210,7 +219,7 @@ def extremumsummary_indexes(observation_set, num_observations):
 
   while summary.size()[0] < num_observations:
     #choose samples to add to summary by finding samples farthest away from samples in current summary
-    _, best_index, original_index = return_furthest_point_index(summary, observation_set) ## here we would like to ensure that we return the original index as well as current index
+    _, best_index, original_index = return_furthest_point_index(summary, observation_set, distance_function=distance_function) ## here we would like to ensure that we return the original index as well as current index
     summary = torch.cat((summary, torch.reshape(observation_set[best_index].get_tensor(), (1,) + individ_obs_size)), dim = 0)
     #observation_set = observation_set[np.r_[:best_index, (best_index + 1):observation_set.size()[0]], ]
     observation_set = observation_set[:best_index] + observation_set[(best_index + 1):]
@@ -218,7 +227,7 @@ def extremumsummary_indexes(observation_set, num_observations):
 
   return summary_indices
 
-def return_furthest_point_index(summary_set, observation_set: IndexedTensor, distance_function = torch.norm):
+def return_furthest_point_index(summary_set, observation_set: IndexedTensor, distance_function = l2_dist):
   '''This function works in more or less an offline fashion. It takes as input an entire summary
   set and the entire set of observations and returns the index of the observation set that is
   farthest away from samples in the current summary. Takes an IndexedTensor as input '''
@@ -247,11 +256,11 @@ def return_furthest_point_index(summary_set, observation_set: IndexedTensor, dis
   return furthest_min_distance, best_index, original_index
 
 
-def calc_pairwise_distances(input_tensor, distance_function = torch.norm):
+def calc_pairwise_distances(input_tensor, distance_function = l2_dist):
     #this should return a matrix with values in the upper top left. probably could be improved but whatever
     length = input_tensor.size()[0]
     distance_matrix = torch.zeros((length+1),(length+1))
     for row in list(range(length)):
         for col in list(range(length - row)):
-            distance_matrix[row,col] = distance_function(input_tensor[row] - input_tensor[col])
+            distance_matrix[row,col] = distance_function(input_tensor[row], input_tensor[col])
     return distance_matrix
