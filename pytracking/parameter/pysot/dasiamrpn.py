a54ff8d14900a2a73e30f2d959c1e8eeb9f1cd18
Levi Cai <cail@mit.edu>
2022-04-20 02:10:06 -0400

    Fixed minor bug with dasiamrpn

--- pytracking/parameter/pysot/dasiamrpn.py
+++ pytracking/parameter/pysot/dasiamrpn.py
@@ -5,7 +5,7 @@ def parameters():
     params = TrackerParams()
 
     params.path_to_pysot_install = "/home/cail/Documents/pysot"
-    params.config_file = "/home/cail/Documents/pysot/experiments/siamrpn_r50_l234_dwxcorr_l/config.yaml"
-    params.model_file = "/home/cail/Documents/pysot/experiments/siamrpn_r50_l234_dwxcorr_l/model.pth"
+    params.config_file = "/home/cail/Documents/pysot/experiments/siamrpn_r50_l234_dwxcorr_lt/config.yaml"
+    params.model_file = "/home/cail/Documents/pysot/experiments/siamrpn_r50_l234_dwxcorr_lt/model.pth"
 
     return params
